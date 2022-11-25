ed0ad155ce6433a3840497dff779ce98c86478f5
Enzo-belardo <vincenzobelardo10@gmail.com>
2022-11-25 17:05:47 +0100

    add: media

--- snacktre.js
+++ snacktre.js
@@ -2,9 +2,12 @@
 
 const number=[4, 6 ,7 , 9 , 12 ,2 ,1 ,13 ,42 ,7]
 let somma = 0;
+let media = 0;
 
 for (let i = 0; i < 10; i++){
-    somma = somma + number [i];
+    somma=somma + number [i];
 }
+media= somma / 10;
+console.log(media);
 
 console.log(somma);
