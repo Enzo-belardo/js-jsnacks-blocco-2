075dcd5e43ba244550edf9c822ebd83487dce71d
Enzo-belardo <vincenzobelardo10@gmail.com>
2022-11-25 15:59:37 +0100

    primo snack

---
+++ script.js
@@ -0,0 +1,19 @@
+//Crea un array vuoto.
+//Chiedi per 6 volte all'utente di inserire un numero,
+//se è dispari inseriscilo nell'array.
+
+let number = ['']
+let numberUser ;
+
+for(let i= 0; i < 6; i++){
+
+    let numberUser= parseInt(prompt('inserisci numero'));
+
+    if(numberUser % 4 == 1 ){
+        numberUser = number 
+    }
+
+}
+
+
+
